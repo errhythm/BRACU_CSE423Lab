cd9e48a3b533b6056e574df11c301d0ea7cbadb7
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-06-07 23:32:13 +0600

    Hi

---
+++ main.py
@@ -0,0 +1,38 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import random
+
+def draw_50points(x, y):
+    for i in range(50):
+        glBegin(GL_POINTS)
+        glVertex2f(x, y)
+        glEnd()
+        x = random.randint(0, 500)
+        y = random.randint(0, 500)
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 1.0, 0.0) #konokichur color set (RGB)
+    #call the draw methods here
+    draw_50points(250, 250)
+    glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500) #window size
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow(b"OpenGL Coding Practice") #window name
+glutDisplayFunc(showScreen)
+
+glutMainLoop()
