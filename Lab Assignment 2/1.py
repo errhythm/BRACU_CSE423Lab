6d1c213317b29de180a8a3a5842a28c26d585218
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-07-04 19:53:28 +0600

    Added

---
+++ Lab Assignment 2/1.py
@@ -0,0 +1,59 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def find_zone(dx, dy):
+    if abs(dx) > abs(dy):
+        if dx >= 0 and dy >= 0:
+            return 0
+        elif dx <= 0 and dy >= 0:
+            return 3
+        elif dx >= 0 and dy <= 0:
+            return 7
+        elif dx <= 0 and dy <= 0:
+            return 4
+    else:
+        if dx >= 0 and dy >= 0:
+            return 1
+        elif dx <= 0 and dy >= 0:
+            return 2
+        elif dx >= 0 and dy <= 0:
+            return 6
+        elif dx <= 0 and dy <= 0:
+            return 5
+
+
+
+def draw_points(x, y):
+    glPointSize(5) #pixel size. by default 1 thake
+    glBegin(GL_POINTS)
+    glVertex2f(x,y) #jekhane show korbe pixel
+    glEnd()
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 1.0, 0.0) #konokichur color set (RGB)
+    #call the draw methods here
+    draw_points(250, 250)
+    glutSwapBuffers()
+
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500) #window size
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow(b"Student ID: 20101298 | Let's Draw: 98") #window name
+glutDisplayFunc(showScreen)
+
+glutMainLoop()
